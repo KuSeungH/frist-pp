7e17c158b3075303ddab80ba42f8b44f9b98737c
KuSeungH <98626004+KuSeungH@users.noreply.github.com>
2022-02-02 23:16:34 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+# test.py
+print('hello, world')
+print('반갑구나 세상아')
+
+# 이 줄은 주석처리 됩니다
+# 주석은 코드로 실행되지 않고, 간단한 메모를 기록하는 공간입니다
+# command + F5 (실행)
+# command + J (터미널열기) python day01/test.py
+
+# IDE : 개발하는데 필요한 여러 기능을 통합적으로 제공해주는 프로그램
+# Intergrated Development Environment (통합 개발 환경)
